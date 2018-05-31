287d7e7a32b32203afe60e8295060bc9eea0292b
jerryngton <jerryngtond@gmail.com>
2018-05-31 11:59:18 +0300

    deficiencies were eliminated

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const RIGHT_ARROW_CODE = 39;
 
 const template = document.querySelector(`#templates`).content;
 const mainFrames = template.querySelectorAll(`.main`);
-const btnArrows = template.querySelector(`.arrows__wrap`)
+const btnArrows = template.querySelector(`.arrows__wrap`);
 const leftButton = template.querySelectorAll(`.arrows__btn`)[0];
 const rightButton = template.querySelectorAll(`.arrows__btn`)[1];
 const appContainer = document.querySelector(`.app`);
@@ -14,28 +14,21 @@ const entryPoint = document.querySelector(`.app .main`);
 appContainer.appendChild(btnArrows);
 let indexFramePointer = 0;
 
-const moveDirection = {
-  "TO_LEFT": `ON_LEFT`,
-  "TO_RIGHT": `ON_RIGHT`,
-};
-
-let getFramePerNumber = (number) => mainFrames[number];
-
 let insertFrameToApp = (number) => {
-  const takenFrame = getFramePerNumber(number);
+  const takenFrame = mainFrames[number];
   if (entryPoint.firstChild) {
     entryPoint.replaceChild(takenFrame, entryPoint.firstChild);
   }
   entryPoint.appendChild(takenFrame);
 };
 
-let changeCurrentFrame = (direction) => {
-  if (direction === moveDirection.TO_LEFT) {
+let changeCurrentFrame = (isDirectionLeft) => {
+  if (isDirectionLeft === true) {
     if (indexFramePointer > 0) {
       --indexFramePointer;
       insertFrameToApp(indexFramePointer);
     }
-  } else if (direction === moveDirection.TO_RIGHT) {
+  } else {
     if (indexFramePointer < mainFrames.length - 1) {
       ++indexFramePointer;
       insertFrameToApp(indexFramePointer);
@@ -47,21 +40,18 @@ insertFrameToApp(indexFramePointer);
 
 document.addEventListener(`keydown`, (evt) => {
   if (evt.keyCode === LEFT_ARROW_CODE) {
-    changeCurrentFrame(moveDirection.TO_LEFT);
+    changeCurrentFrame(true);
   } else if (evt.keyCode === RIGHT_ARROW_CODE) {
-    changeCurrentFrame(moveDirection.TO_RIGHT);
+    changeCurrentFrame(false);
   }
 });
 
 leftButton.addEventListener(`click`, () => {
-  changeCurrentFrame(moveDirection.TO_LEFT);
+  changeCurrentFrame(true);
 });
 
 rightButton.addEventListener(`click`, () => {
-  changeCurrentFrame(moveDirection.TO_RIGHT);
+  changeCurrentFrame(false);
 });
-
-
-
 
 
