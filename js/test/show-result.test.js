eeb1c724c326d24431c156a2fb6fefd823ce2d02
jerryngton <jerryngtond@gmail.com>
2018-06-27 20:44:50 +0300

    Test were fixed

--- js/test/show-result.test.js
+++ js/test/show-result.test.js
@@ -5,28 +5,57 @@ const STATISTICS = [
   {
     scores: 25,
     remainNotes: 2,
-    remainTimes: 22
+    remainTimes: 22,
+    id: 120
   },
   {
     scores: 14,
     remainNotes: 2,
-    remainTimes: 22
+    remainTimes: 22,
+    id: 121
   },
   {
     scores: 1,
     remainNotes: 2,
-    remainTimes: 22
+    remainTimes: 22,
+    id: 122
   },
   {
     scores: 4,
     remainNotes: 2,
-    remainTimes: 22
+    remainTimes: 22,
+    id: 123
   },
   {
     scores: 6,
     remainNotes: 2,
-    remainTimes: 22
+    remainTimes: 22,
+    id: 124
   },
+  {
+    scores: 8,
+    remainNotes: 2,
+    remainTimes: 22,
+    id: 125
+  },
+  {
+    scores: 100,
+    remainNotes: 2,
+    remainTimes: 22,
+    id: 126
+  },
+  {
+    scores: 1,
+    remainNotes: 2,
+    remainTimes: 22,
+    id: 127
+  },
+  {
+    scores: 8,
+    remainNotes: 2,
+    remainTimes: 22,
+    id: 128
+  }
 ];
 
 describe(`Result page`, () => {
@@ -35,28 +64,23 @@ describe(`Result page`, () => {
       assert.equal(getComparison(STATISTICS, {
         scores: 8,
         remainNotes: 2,
-        remainTimes: 22
-      }), `Вы заняли 3-ое место из 6. Это лучше чем у 50% игроков.`);
+        remainTimes: 22,
+        id: 125
+      }), `Вы заняли 4-ое место из 9. Это лучше чем у 56% игроков.`);
 
       assert.equal(getComparison(STATISTICS, {
         scores: 100,
         remainNotes: 2,
-        remainTimes: 22
-      }), `Вы заняли 1-ое место из 6. Это лучше чем у 83% игроков.`);
+        remainTimes: 22,
+        id: 126
+      }), `Вы заняли 1-ое место из 9. Это лучше чем у 89% игроков.`);
 
       assert.equal(getComparison(STATISTICS, {
         scores: 1,
         remainNotes: 2,
-        remainTimes: 22
-      }), `Вы заняли 6-ое место из 6. Это лучше чем у 0% игроков.`);
-    });
-
-    it(`get position and persentage if statistics are empty`, () => {
-      assert.equal(getComparison([], {
-        scores: 8,
-        remainNotes: 2,
-        remainTimes: 22
-      }), `Вы заняли 1-ое место из 1. Это лучше чем у 0% игроков.`);
+        remainTimes: 22,
+        id: 127
+      }), `Вы заняли 9-ое место из 9. Это лучше чем у 0% игроков.`);
     });
   });
 });
