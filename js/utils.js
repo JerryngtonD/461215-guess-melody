dee552837e2be196d38695435d9a4cd73a7bbc91
jerryngton <jerryngtond@gmail.com>
2018-06-27 20:30:07 +0300

    Function which generated ID was added

--- js/utils.js
+++ js/utils.js
@@ -31,3 +31,10 @@ export const mixArray = (arr) => {
   }
   return arr;
 };
+
+
+export const randomInteger = (min, max) => {
+  let rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
+};
